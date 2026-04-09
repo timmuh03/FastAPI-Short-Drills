56a473f3571d2dfdf20dcb6c8605e7839582e953
Timothy Muhlsetein <122745865+timmuh03@users.noreply.github.com>
2026-04-08 22:49:02 -0400

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,23 @@
 
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./app.db"
+engine = create_engine(
+  DATABASE_URL,
+  connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+  autocommit=False,
+  autoflush=False,
+  bind=engine
+)
+
+def check_db_connection() -> bool:
+  try:
+    with engine.connect() as connection:
+      connection.execute(text("SELECT 1"))
+    return True
+  except Exception:
+    return False
