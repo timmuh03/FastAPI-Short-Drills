a848fe2e1966ad007e0bd9d87fd9be1478faa442
Timothy Muhlsetein <122745865+timmuh03@users.noreply.github.com>
2026-04-08 22:59:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from fastapi import FastAPI
+from db import check_db_connection
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+  return {"message": "FastAPI is running")
+
+@app.get("/health")
+def health_check():
+  return {"status": "ok"}
+
+@app.get("/health/db")
+def db_health_check():
+  db_ok = check_db_connection()
+
+  if db_ok:
+    return {
+      "status": "ok",
+      "database": "connected"
+    }
+
+  return {
+    "status": "error",
+    "database": "not connected"
+  }
